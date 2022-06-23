b48130dcc4dc39e5d3aa89efdd699c83decc52fe
Sera4696 <k019g1549@gmail.com>
2022-06-23 09:43:27 +0900

    床の消滅処理

---
+++ Assets/Scripts/FloorDelete.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorDelete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            Debug.Log("通り抜け終えた");
+            Destroy(other.gameObject);
+        }
+       
+    }
+}
