bcf98d83487047e6bb2ed8d8a86ff03b3d4fefa5
Sera4696 <k019g1549@gmail.com>
2022-07-14 10:37:20 +0900

    パネルの生成処理

---
+++ Assets/Scripts/InsFloor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsFloor : MonoBehaviour
+{
+    [SerializeField] GameObject insFloor;
+    [SerializeField] GameObject[] preFloors;
+    [SerializeField] int floorCount;
+    [SerializeField] int insCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        insCount = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (insFloor == null && insCount == 3)
+        {
+            floorCount = Random.Range(0, 4);
+            insFloor = Instantiate(preFloors[floorCount], transform.position, Quaternion.identity);
+            insCount = 0;
+        }
+
+        if (insFloor == null &&
+            (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || 
+            Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)))
+        {
+            insCount += 1;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] public int moveDistance;
+    [SerializeField] public float gravity;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
         moveDistance = 2;
+        gravity = -0.08f;
     }
 
     // Update is called once per frame
@@ -21,26 +23,38 @@ public class Player : MonoBehaviour
     //Playerの移動処理
     private void Move()
     {
-
+        //transform.position += new Vector3(0, gravity, 0);
         if (Input.GetKeyDown(KeyCode.W))
         {
             transform.position += new Vector3(0, 0, moveDistance);
+            gravity = -0.08f;
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
             transform.position -= new Vector3(0, 0, moveDistance);
+            gravity = -0.08f;
         }
 
         if (Input.GetKeyDown(KeyCode.A))
         {
             transform.position -= new Vector3(moveDistance, 0, 0);
+            gravity = -0.08f;
         }
-        
+
         if (Input.GetKeyDown(KeyCode.D))
         {
             transform.position += new Vector3(moveDistance, 0, 0);
+            gravity = -0.08f;
         }
 
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "FR" || other.gameObject.tag == "FB" ||
+            other.gameObject.tag == "FG" || other.gameObject.tag == "FY")
+        {
+            gravity = 0;
+        }
     }
 }
