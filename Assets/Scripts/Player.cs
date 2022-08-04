e09f4681b76c882af60376161caaa751594e45ec
WATANABEH-MPC\ec <k019g1549@g.neec.ac.jp>
2022-08-04 18:02:18 +0900

    一応仕上げたよ

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,27 @@ public class Player : MonoBehaviour
     [SerializeField] public float gravity;
 
     [SerializeField] public float moveCount;
+    [SerializeField] public float deadCount;
 
     [SerializeField] public static int ScoreCount;
 
-    [SerializeField] public GameObject Score;
-    [SerializeField] public GameObject UIBack;
+    [SerializeField] public bool playerDead;
+    [SerializeField] public bool isStart;
 
+    [SerializeField] public GameObject Score;
+    [SerializeField] public GameObject Result;
+    [SerializeField] public GameObject RScore;
+    [SerializeField] public GameObject Retry;
+    [SerializeField] public GameObject Title;
+    [SerializeField] public GameObject StartT;
+    [SerializeField] public GameObject UIBack;
+    [SerializeField] public GameObject StarL;
+    [SerializeField] public GameObject StarS;
+    [SerializeField] public GameObject StarR;
+
+    public AudioClip sound1;
+    public AudioClip sound2;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +39,18 @@ public class Player : MonoBehaviour
         moveDistance = 2;
         gravity = -0.08f;
         moveCount = 50;
+        UIBack.SetActive(false);
+        Result.SetActive(false);
+        RScore.SetActive(false);
+        Retry.SetActive(false);
+        StarL.SetActive(false);
+        StarS.SetActive(false);
+        StarR.SetActive(false);
+        deadCount = 0;
+        playerDead = false;
+        isStart = false;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -47,6 +74,8 @@ public class Player : MonoBehaviour
                 transform.position += new Vector3(0, 2, moveDistance);
                 gravity = -0.08f;
                 moveCount = 0;
+                isStart = true;
+                audioSource.PlayOneShot(sound1);
             }
 
             if (Input.GetKeyDown(KeyCode.S))
@@ -54,6 +83,8 @@ public class Player : MonoBehaviour
                 transform.position -= new Vector3(0, -2, moveDistance);
                 gravity = -0.08f;
                 moveCount = 0;
+                isStart = true;
+                audioSource.PlayOneShot(sound1);
             }
 
             if (Input.GetKeyDown(KeyCode.A))
@@ -61,6 +92,8 @@ public class Player : MonoBehaviour
                 transform.position -= new Vector3(moveDistance, -2, 0);
                 gravity = -0.08f;
                 moveCount = 0;
+                isStart = true;
+                audioSource.PlayOneShot(sound1);
             }
 
             if (Input.GetKeyDown(KeyCode.D))
@@ -68,6 +101,8 @@ public class Player : MonoBehaviour
                 transform.position += new Vector3(moveDistance, 2, 0);
                 gravity = -0.08f;
                 moveCount = 0;
+                isStart = true;
+                audioSource.PlayOneShot(sound1);
             }
         }
 
@@ -75,17 +110,81 @@ public class Player : MonoBehaviour
 
     public void UI()
     {
+
         Text score_text = Score.GetComponent<Text>();
 
+        Text result_text = Result.GetComponent<Text>();
+        Text rscore_text = RScore.GetComponent<Text>();
+        Text retry_text = Retry.GetComponent<Text>();
+
         score_text.text = "Score:" + ScoreCount;
+        rscore_text.text = "" + ScoreCount;
+
+        if (isStart)
+        {
+            Title.transform.position += new Vector3(0, 1.5f, 0);
+            StartT.transform.position += new Vector3(0, -1.5f, 0);
+        }
+
+        if (playerDead)
+        {
+            deadCount++;
+        }
+
+        if (deadCount > 50)
+        {
+            UIBack.SetActive(true);
+        }
+
+        if (deadCount > 100)
+        {
+            Result.SetActive(true);
+        }
+
+        if (deadCount > 150)
+        {
+            RScore.SetActive(true);
+        }
+
+        if (deadCount > 200)
+        {
+            if (ScoreCount >= 25)
+            {
+                StarL.SetActive(true);
+            }
+
+            if (ScoreCount >= 50)
+            {
+                StarL.SetActive(true);
+                StarS.SetActive(true);
+            }
+
+            if (ScoreCount >= 100)
+            {
+                StarL.SetActive(true);
+                StarS.SetActive(true);
+                StarR.SetActive(true);
+            }
+        }
+
+        if (deadCount > 250)
+        {
+            Retry.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
     }
 
     void Dead()
     {
-        if (transform.position.y < -2.5f)
+        if (transform.position.y < -2.0f)
         {
+            audioSource.PlayOneShot(sound2);
+            playerDead = true;
             Debug.Log("死");
-            Destroy(gameObject);
+            //gameObject.SetActive(false);
         }
     }
 
